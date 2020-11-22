0b989bfa9ed30728de51cd7b7a0fc9c88e56320f
v <vladimirfol@gmail.com>
2020-11-22 03:03:41 +0300

    add load queues

--- main.js
+++ main.js
@@ -23,66 +23,27 @@ let market = await Market.load(connection, marketAddress, undefined, programAddr
 // Fetching orderbooks
 let bids = await market.loadBids(connection);
 let asks = await market.loadAsks(connection);
+
 // L2 orderbook data
-for (let [price, size] of bids.getL2(20)) {
+console.log('bids top5:');
+for (let [price, size] of bids.getL2(5)) {
     console.log(price, size);
 }
+
 // Full orderbook data
 for (let order of asks) {
     console.log(
-        order.orderId,
+        order.side,
         order.price,
         order.size,
-        order.side, // 'buy' or 'sell'
+        order.orderId,
     );
 }
 
-// Placing orders
-let owner = new Account('...');
-let payer = new PublicKey('...'); // spl-token account
-await market.placeOrder(connection, {
-    owner,
-    payer,
-    side: 'buy', // 'buy' or 'sell'
-    price: 123.45,
-    size: 17.0,
-    orderType: 'limit', // 'limit', 'ioc', 'postOnly'
-});
+let events = await market.loadEventQueue(connection)
+console.log('events: ', events);
 
-// Retrieving open orders by owner
-let myOrders = await market.loadOrdersForOwner(connection, owner.publicKey);
+let requests = await market.loadRequestQueue(connection)
+console.log('requests: ', requests);
 
-// Cancelling orders
-for (let order of myOrders) {
-    await market.cancelOrder(connection, owner, order);
-}
-
-// Retrieving fills
-for (let fill of await market.loadFills(connection)) {
-    console.log(
-        fill.orderId,
-        fill.price,
-        fill.size,
-        fill.side,
-    );
-}
-
-// Settle funds
-for (let openOrders of await market.findOpenOrdersAccountsForOwner(
-    connection,
-    owner.publicKey,
-)) {
-    if (openOrders.baseTokenFree > 0 || openOrders.quoteTokenFree > 0) {
-        // spl-token accounts to which to send the proceeds from trades
-        let baseTokenAccount = new PublicKey('...');
-        let quoteTokenAccount = new PublicKey('...');
-
-        await market.settleFunds(
-            connection,
-            owner,
-            openOrders,
-            baseTokenAccount,
-            quoteTokenAccount,
-        );
-    }
-}
+market.
